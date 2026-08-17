bee163c1b09c72a8bf93809f17a65f9d85dbd3ec
vittor <vittor.moreno@escola.pr.gov.br>
2026-08-16 21:26:18 -0300

    feat: adiciona a tela e as funcionalidades do login

---
+++ src/app/components/layout/login/login.ts
@@ -0,0 +1,25 @@
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
+
+@Component({
+  selector: 'app-login',
+  imports: [MdbFormsModule, FormsModule],
+  templateUrl: './login.html',
+  styleUrl: './login.scss',
+})
+export class Login {
+  usuario!: string;
+  senha!: string;
+
+  router = inject(Router);
+
+  logar(){
+    if(this.usuario == "admin" && this.senha == "admin123"){
+      this.router.navigate(['admin/carros']);
+    }else{
+      alert('Usuário ou senha incorretos!');
+    }
+  }
+}
