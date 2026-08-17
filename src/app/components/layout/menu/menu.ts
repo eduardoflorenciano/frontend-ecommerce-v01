77c06ea6262fd956657e206487f47bc0814d90e5
vittor <vittor.moreno@escola.pr.gov.br>
2026-08-16 21:31:46 -0300

    feat: criação de menu e principal

---
+++ src/app/components/layout/menu/menu.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { MdbCollapseModule } from 'mdb-angular-ui-kit/collapse';
+
+@Component({
+  selector: 'app-menu',
+  imports: [MdbCollapseModule],
+  templateUrl: './menu.html',
+  styleUrl: './menu.scss',
+})
+export class Menu {}
