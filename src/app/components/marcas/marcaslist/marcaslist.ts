f3f8c18a6bbd6f1639ebba8a51ceff95ef11030c
Lucas Eduardo <lucaszarzabarros@gmail.com>
2026-08-16 21:44:57 -0300

    feat: adiciona os arquivos que fazem a listagem de marcas

---
+++ src/app/components/marcas/marcaslist/marcaslist.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-marcaslist',
+  imports: [],
+  templateUrl: './marcaslist.html',
+  styleUrl: './marcaslist.scss',
+})
+export class Marcaslist {}
